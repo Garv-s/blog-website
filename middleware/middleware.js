8ee139c9a3a4ea5d7621641d8f54fc9dde3d33da
Garv-s <garvnilesh18@gmail.com>
2025-08-27 00:09:53 +0530

    added middleware for verification and protected routes

---
+++ middleware/middleware.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const verify = async (req, res,next) => {
+    const header = req.headers['authorization'];
+    const token = header && header.split(" ")[1];
+    console.log(token);
+    
+    if(!token) return res.status(401).json({ message: 'No token provided' });
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_KEY);
+        //console.log(decoded);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        //console.log(error);
+        return res.status(403).json({ message: 'Invalid or expired token' });
+
+    }
+
+
+}
